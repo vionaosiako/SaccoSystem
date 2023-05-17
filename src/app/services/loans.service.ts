8598afbc40ee270a9d72a304dfd9ddff214373f7
vionaosiako <viona.osiako@student.moringaschool.com>
2023-05-17 12:39:36 +0300

    Import observable, HttpHeaders and HttpClient

--- src/app/services/loans.service.ts
+++ src/app/services/loans.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoansService {
+  baseurl = 'http://127.0.0.1:8000/';
 
   constructor() { }
 }
