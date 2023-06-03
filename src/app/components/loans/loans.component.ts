656b2a5cfd6a5ae398b2834aea3b35bdb709bb3e
vionaosiako <viona.osiako@student.moringaschool.com>
2023-06-03 16:29:43 +0300

    commeny out codes

--- src/app/components/loans/loans.component.ts
+++ src/app/components/loans/loans.component.ts
@@ -15,60 +15,7 @@ export class LoansComponent implements OnInit {
 
   ngOnInit(): void {
   }
-// -------------------------------------------------------------------------------------------------------------------------------------
-// Loan Request
-// -------------------------------------------------------------------------------------------------------------------------------------
 
-  getLoanRequest = () => {
-    this.api.getLoanRequest().subscribe(
-      (data) => {
-        this.loans = data;
-        setTimeout(()=>{   
-          $('#datatableexample').DataTable( {
-            pagingType: 'full_numbers',
-            pageLength: 5,
-            processing: true,
-            lengthMenu : [5, 10, 25],
-            responsive: true, // Use this attribute to enable the responsive extension
-        } 
-        );
-        }, 1);
-              }, error => console.error(error));
-  }
-
-  newLoanRequest(){
-    this.api.createLoanRequest(this.loans).subscribe(
-      response=> {
-        alert('Loan Requested Successfully!')
-        this.getLoanRequest()
-      },
-      error=> console.log (error)
-    );
-
-}
-
-  putLoanRequest = () => {
-    this.api.updateLoanRequest(this.loans).subscribe(
-      response=> {
-        alert('Loan Request Successfully!')
-        this.getLoanRequest()
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  }
-
-  loanRequestClicked = (request:any) => {
-    this.api.getOneLoanRequest(request.id).subscribe(
-      data => {
-        this.loans = data;
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  }
 // -------------------------------------------------------------------------------------------------------------------------------------
 // Loan category
 // -------------------------------------------------------------------------------------------------------------------------------------
